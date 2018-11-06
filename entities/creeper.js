fe09a85c3535efca4035c5e4eff70ad92d115089
Raxkas <rxrhhx@gmail.com>
2018-11-06 13:36:30 +0300

    Make Creeper.bang radius customizable

--- entities/creeper.js
+++ entities/creeper.js
@@ -7,6 +7,10 @@ class Creeper extends Attacker {
         return this.kind._bangCost;
     }
 
+    get _bangRadius() {
+        return this.kind._bangRadius;
+    }
+
     _nextTick() {
         if (this.energy >= this._bangCost) {
             this.bang()
@@ -27,11 +31,26 @@ class Creeper extends Attacker {
         }
     }
 
+    _getArea(radius) {
+        let [x, y] = this.pos;
+        let rangePositive = [];
+        for (let i = 1; i <= radius; i++) {
+            rangePositive.push(i);
+        }
+        let rangeNegative = rangePositive.map(v => -v).reverse();
+        let range = [].concat(rangeNegative, [0], rangePositive);
+        let xRange = range.map(v => x + v);
+        let yRange = range.map(v => y + v);
+        let rows = yRange.map(y => xRange.map(x => [x, y]));
+        let area = [].concat(...rows).filter(p => this._gameLogic.isPosCorrect(p));
+        return area;
+    }
+
     bang() {
         if (this.energy < this._bangCost) {
             throw "Not enough energy";
         }
-        let damaged = this.directions.concat([this.pos]);
+        let damaged = this._getArea(this._bangRadius);
         damaged.map(p => this.kill(p));
     }
 }
@@ -41,3 +60,4 @@ Creeper._multiplicationCost = +Infinity;
 Creeper._energyLimit = +Infinity;
 Creeper._energyFromPrey = 2;
 Creeper._bangCost = 20;
+Creeper._bangRadius = 5;
