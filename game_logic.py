ae52e2701ea922ed201fbdebf89de8da2b297779
Raxkas <rxrhhx@gmail.com>
2018-11-22 21:40:38 +0300

    Translate AGOLLogic

--- game_logic.py
+++ game_logic.py
@@ -1,144 +1,105 @@
-function _getRandomOf(kinds, spawnChances) {
-    const chancesSum = spawnChances.reduce((a, b) => a+b);
-    const seed = Math.random() * chancesSum;
-    let passedChancesSum = 0;
-    for (var id in spawnChances) {
-        passedChancesSum += spawnChances[id];
-        if (passedChancesSum > seed) {
-            return kinds[id];
-        }
-    }
-    throw "Something went wrong. Arguments: " + [kinds, spawnChances];
-}
+from collections.abc import Sequence
+from random import random
 
 
-class AGOLLogic {
-    constructor(width, height, kinds, spawnChances) {
-        this.width = width;
-        this.height = height;
-        this._KINDS = Array.from(kinds);
-        this._ARRAYS = [];
-        for (let i in this._KINDS) {
-            this._ARRAYS.push([]);
-        }
-        this._matrix = [];
-        for (let y = 0; y < this.height; y++) {
-            this._matrix.push([]);
-            for (let x = 0; x < this.width; x++) {
-                this._matrix[y].push(null);
-            }
-        }
-        this._generateEntities(spawnChances);
-        this.tickNumber = 0;
-    }
-    
-    _generateEntities(spawnChances) {
-        for (let y = 0; y < this.height; y++) {
-            for (let x = 0; x < this.width; x++) {
-                let kind = _getRandomOf(this._KINDS, spawnChances);
-                this.replace([x,y], kind);
-            }
-        }
-    }
-    
-    nextTick() {
-        let entities = [].concat(...this._ARRAYS);
-        for (let entity of entities) {
-            if (entity.alive) {
-                entity.nextTick();
-            }
-        }
-        this.tickNumber += 1;
-    }
-    
-    isPosCorrect(pos) {
-        let [x, y] = pos;
-        return (0 <= x && x < this.width && 0 <= y && y < this.height);
-    }
-    
-    getEntityByPos(pos) {
-        if (!this.isPosCorrect(pos)) {
-            throw "Incorrect pos: " + pos;
-        }
-        let [x, y] = pos;
-        let entity = this._matrix[y][x];
-        return entity;
-    }
-    
-    replace(value, kind) {
-        let oldEntity = this._getEntityBy(value);
-        
-        if (value === oldEntity) {
-            var pos = oldEntity.pos;
-        }
-        else {
-            var pos = value;
-        }
-        let [x, y] = pos;
-        
-        if (oldEntity !== null) {
-            this._matrix[y][x] = null;
-            let oldEntityArray = this.getArrayBy(oldEntity);
-            let i = oldEntityArray.indexOf(oldEntity);
-            if (i < 0) {
-                throw "Entity not in array";
-            }
-            oldEntityArray.splice(i, 1);
-            oldEntity.alive = false;
-        }
-        
-        let newEntity = new kind();
-        this.getArrayBy(newEntity).push(newEntity);
-        this._matrix[y][x] = newEntity;
-        newEntity.pos = pos;
-        newEntity._gameLogic = this;
-        newEntity.alive = true;
-    }
-    
-    swap(value1, value2) {
-        let [entity1, entity2] = [value1, value2].map(v => this._getEntityBy(v));
-        let [pos1, pos2] = [entity1.pos, entity2.pos];
-        [entity1.pos, entity2.pos] = [pos2, pos1];
-        let [[x1, y1], [x2, y2]] = [pos1, pos2];
-        [this._matrix[y1][x1], this._matrix[y2][x2]] = [entity2, entity1];
-    }
-    
-    getArrayBy(value) {
-        let id = this._getId(value);
-        return this._ARRAYS[id];
-    }
-    
-    _getId(value) {
-        if (this._ARRAYS.includes(value)) {
-            return this._ARRAYS.indexOf(value);
-        }
-        else if (value instanceof Array && value.length == 2) {
-            return this.getEntityByPos(value)
-        }
-        else if (this._KINDS.includes(value)) {
-            return this._KINDS.indexOf(value);
-        }
-        else if (this._KINDS.includes(value.constructor)) {
-            return this._getId(value.constructor);
-        }
-        else {
-            throw "Incorrect value: " + value;
-        }
-    }
-    
-    _getEntityBy(value) {
-        if (this._KINDS.includes(value.constructor)) {
-            if (!value.alive) {
-                throw "Dead entity";
-            }
-            return value;
-        }
-        else {
-            return this.getEntityByPos(value);
-        }
-    }
-    
-    count(kind) {
-        return this.getArrayBy(kind).length;
-    }
-}
+# TODO: use itertools.accumulate
+def _getRandomOf(kinds, spawnChances):
+    assert len(kinds) == len(spawnChances)
+    chancesSum = sum(spawnChances)
+    seed = random() * chancesSum
+    passedChancesSum = 0
+    for kind, chance in zip(kinds, spawnChances):
+        passedChancesSum += chance
+        if passedChancesSum > seed:
+            return kind
+
+
+class AGOLLogic:
+    def __init__(self, width, height, kinds, spawnChances):
+        self.width = width
+        self.height = height
+        self._KINDS = tuple(kinds)
+        self._ARRAYS = tuple(list() for kind in self._KINDS)
+        self._matrix = []
+        for y in range(self.height):
+            row = [None] * self.width
+            self._matrix.append(row)
+        self._generateEntities(spawnChances)
+        self.tickNumber = 0
+
+    def _generateEntities(self, spawnChances):
+        for y in range(self.height):
+            for x in range(self.width):
+                kind = _getRandomOf(self._KINDS, spawnChances)
+                self.replace((x, y), kind)
+
+    def nextTick(self):
+        entities = sum(self._ARRAYS, [])
+        for entity in entities:
+            if entity.alive:
+                entity.next_tick()
+        self.tickNumber += 1
+
+    def isPosCorrect(self, pos):
+        x, y = pos
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def getEntityByPos(self, pos):
+        if not self.isPosCorrect(pos):
+            raise ValueError("Incorrect pos: " + pos)
+        x, y = pos
+        return self._matrix[y][x]
+
+    def replace(self, value, kind):
+        oldEntity = self._getEntityBy(value)
+        if value is oldEntity:
+            pos = oldEntity.pos
+        else:
+            pos = value
+        x, y = pos
+
+        if oldEntity is not None:
+            self._matrix[y][x] = None
+            self.getArrayBy(oldEntity).remove(oldEntity)
+            oldEntity.alive = False
+
+        newEntity = kind()
+        newEntity.pos = pos
+        newEntity._game_logic = self
+        self._matrix[y][x] = newEntity
+        self.getArrayBy(newEntity).append(newEntity)
+        newEntity.alive = True
+
+    def swap(self, value1, value2):
+        entity1, entity2 = map(self._getEntityBy, [value1, value2])
+        pos1, pos2 = entity1.pos, entity2.pos
+        entity1.pos, entity2.pos = pos2, pos1
+        (x1, y1), (x2, y2) = pos1, pos2
+        self._matrix[y1][x1], self._matrix[y2][x2] = entity2, entity1
+
+    def getArrayBy(self, value):
+        id = self._getId(value)
+        return self._ARRAYS[id]
+
+    def _getId(self, value):
+        if value in self._ARRAYS:
+            return self._ARRAYS.index(value)
+        elif value in self._KINDS:
+            return self._KINDS.index(value)
+        elif type(value) in self._KINDS:
+            return self._KINDS.index(type(value))
+        elif isinstance(value, Sequence):
+            return self.getEntityByPos(value)
+        else:
+            raise TypeError("Incorrect value: %s" % value)
+
+    def _getEntityBy(self, value):
+        if type(value) in self._KINDS:
+            if not value.alive:
+                raise AssertionError("Dead entity")
+            return value
+        else:
+            return self.getEntityByPos(value)
+
+    def count(self, kind):
+        return len(self.getArrayBy(kind))
