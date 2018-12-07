a4525b0bf36811753a9cb79e70b3430e4f029bf9
Raxkas <rxrhhx@gmail.com>
2018-12-07 19:53:10 +0300

    Begin use the kivy.graphics.texture.Texture instead of the entity-by-entity drawing

--- field_widget.py
+++ field_widget.py
@@ -1,10 +1,12 @@
 from kivy.uix.widget import Widget
-from kivy.graphics import Color, Rectangle
+from kivy.graphics.texture import Texture
+from kivy.graphics import Rectangle
 
 from entities.mob import Mob
 
 
 class FieldWidget(Widget):
+    # TODO: move to constructor
     COLORS = {
         "Air": (1, 1, 1, 0),
         "Grass": (0, 0.5, 0),
@@ -13,32 +15,43 @@ class FieldWidget(Widget):
         "Creeper": (0, 0.25, 0.75),
         "Monorem": (1, 1, 1)
     }
+    _agol_texture = None
 
     def __init__(self, agol_logic, **kwargs):
         self._agol_logic = agol_logic
         super().__init__(**kwargs)
+        self.bind(size=lambda *args: self._update_canvas())
 
     def update(self):
-        self.canvas.clear()
-        for y in range(self._agol_logic.height):
-            for x in range(self._agol_logic.width):
+        size = self._agol_logic.size
+        texture = Texture.create(size=size)
+        texture.mag_filter = 'nearest'
+        buf = [None] * (4 * size.x*size.y)
+        for y in range(size.y):
+            for x in range(size.x):
                 entity = self._agol_logic.get_entity_by_pos((x, y))
-                self._draw_entity(entity)
+                color = self._get_color_by_entity(entity)
+                color = map(lambda k: int(k*255), color)
+                i = x + y*size.x
+                start, end = 4*i, 4*(i+1)
+                buf[start:end] = color
+        buf = bytes(buf)
+        texture.blit_buffer(buf, colorfmt='rgba')
+        self._agol_texture = texture
+        self._update_canvas()
 
-    def _draw_entity(self, entity):
+    def _update_canvas(self):
+        self.canvas.clear()
+        if self._agol_texture is None:
+            return;
         cell_side_px = min(
-            self.size[0] / self._agol_logic.width,
-            self.size[1] / self._agol_logic.height
+            self.size[0] / self._agol_logic.size.x,
+            self.size[1] / self._agol_logic.size.y
         )  # cell must have square shape
-        cell_sides_px = (cell_side_px, cell_side_px)
-        x, y = entity.pos
-        color = self._get_color_by_entity(entity)
-        x_pos_px = self.pos[0] + x * cell_sides_px[0]
-        y_pos_px = self.pos[1] + y * cell_sides_px[1]
-        pos_px = (x_pos_px, y_pos_px)
+        rect_size = (cell_side_px * self._agol_logic.size.x,
+                     cell_side_px * self._agol_logic.size.y)
         with self.canvas:
-            Color(*color)
-            Rectangle(pos=pos_px, size=cell_sides_px)
+            Rectangle(pos=self.pos, size=rect_size, texture=self._agol_texture)
 
     @classmethod
     def _get_color_by_entity(cls, entity):
