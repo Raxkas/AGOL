0ad8ac6c9a03d1e9bc3ee4fafb87718db9e91fc9
Raxkas <rxrhhx@gmail.com>
2018-12-15 00:35:39 +0300

    Optimize and refactor Entity.is_near()

--- entities/entity.py
+++ entities/entity.py
@@ -21,10 +21,14 @@ class Entity(metaclass=ABCMeta):
     def _get_area(self, radius):
         return self._game_logic.get_region_points(self.pos, radius)
 
-    def is_near(self, *args):
-        return len(self.find_near(*args)) > 0
+    def is_near(self, *kinds_needed):
+        mobs_near = self.find_near()
+        kinds_near = frozenset(map(type, mobs_near))
+        return any(map(lambda kind: issubclass(kind, kinds_needed), kinds_near))
 
     def find_near(self, *kinds):
         mobs_near = map(self._game_logic.get_entity_by_pos, self.directions)
+        if not kinds:
+            return tuple(mobs_near)
         found = filter(lambda mob: isinstance(mob, kinds), mobs_near)
         return tuple(found)
