2f2546267635be0120ff78a27915bc84f2efa449
Raxkas <rxrhhx@gmail.com>
2018-12-16 00:36:21 +0300

    Begin use lines better

--- graph_widget.py
+++ graph_widget.py
@@ -14,38 +14,43 @@ class GraphWidget(Widget):
         self.bind(size=lambda *args: self._reload_canvas())
         self.bind(pos=lambda *args: self._reload_canvas())
 
-    def update(self):
-        self._population_history.append(self._get_population_info())
-        last_x = (len(self._population_history)-1) % self.size[0]
-        last_x = int(last_x)
-        if last_x == 0:
-            self.canvas.clear()
+    def _init_graphs(self):
+        self._graphs = {}
         for kind in self._agol_logic.kinds:
-            self._draw_one_line(indices=[-2, -1], kind=kind)
+            color = self._get_color_by_kind(kind)
+            with self.canvas:
+                Color(*color)
+                self._graphs[kind] = Line(points=(), width=1)
+
+    def update(self):
+        if not hasattr(self, "_graphs"):
+            self._init_graphs()
+        self._population_history.append(self._get_population_info())
+        last_x = self._get_x_by_history_index(-1)
+        if int(last_x) == int(self.pos[0]):
+            self.canvas.clear()
+            self._init_graphs()
+        for kind in self._agol_logic.kinds:
+            self._add_point(index=-1, kind=kind)
 
     def _reload_canvas(self):
         self.canvas.clear()
+        self._init_graphs()
         history_length = len(self._population_history)
-        lines_count = int(history_length % self.size[0])
-        lines_ends_indices_range = range(
-            history_length - lines_count,
+        points_count = history_length % int(self.size[0])
+        history_indices_range = range(
+            history_length - points_count,
             history_length
         )
-        for index2 in lines_ends_indices_range:
-            index1 = index2 - 1 if index2 != 0 else index2
+        for index in history_indices_range:
             for kind in self._agol_logic.kinds:
-                self._draw_one_line(indices=[index1, index2], kind=kind)
+                self._add_point(index=index, kind=kind)
 
-    def _draw_one_line(self, *, indices, kind):
-        x1, x2 = map(self._get_x_by_history_index, indices)
-        if x1 > x2:
-            x1 = x2
-        counts = map(lambda index: self._population_history[index][kind], indices)
-        y1, y2 = map(self._get_y_by_kind_count, counts)
-        color = self._get_color_by_kind(kind)
-        with self.canvas:
-            Color(*color)
-            Line(points=[(x1, y1), (x2, y2)], width=1)
+    def _add_point(self, *, index, kind):
+        x = self._get_x_by_history_index(index)
+        count = self._population_history[index][kind]
+        y = self._get_y_by_kind_count(count)
+        self._graphs[kind].points += (x, y)
 
     def _get_population_info(self):
         keys = tuple(self._agol_logic.kinds)
@@ -57,7 +62,8 @@ class GraphWidget(Widget):
         if index < 0:
             index = len(self._population_history) + index
             assert index >= 0
-        x = index % self.size[0]
+        x = index
+        x %= int(self.size[0])
         return self.pos[0] + x
 
     def _get_y_by_kind_count(self, count):
