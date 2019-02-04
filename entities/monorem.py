547fa323312d3354fa88f6c0eb73bed977b66fc8
Raxkas <rxrhhx@gmail.com>
2019-02-05 00:11:49 +0300

    Remove the Monorem's common energy feature

--- entities/monorem.py
+++ entities/monorem.py
@@ -5,15 +5,11 @@ from entities.grass import Grass
 from entities.attacker import Attacker
 
 
-# TODO: remove global value
-_monorems_joint_energy = 0
-
-
 class Monorem(Attacker):
     __slots__ = ()
 
-    _default_energy = 5
-    _energy_limit = 20
+    _default_energy = 15
+    _energy_limit = 40
     _energy_increment_per_tick = 1
     _movement_cost = 0
     _energy_from_prey = None
@@ -36,18 +32,3 @@ class Monorem(Attacker):
         if kind is Grass:
             return self.energy >= 2*self._default_energy + self._energy_increment_per_tick
         return super()._do_multiply_on(kind)
-
-    @property
-    def energy(self):
-        if not hasattr(self, "_game_logic"):
-            return self._default_energy
-        return _monorems_joint_energy/self._game_logic.count_kind(type(self))
-
-    @energy.setter
-    def energy(self, value):
-        global _monorems_joint_energy
-        _monorems_joint_energy -= self.energy
-        _monorems_joint_energy += value
-        if hasattr(self, "_game_logic"):
-            if self.energy > self._energy_limit:
-                _monorems_joint_energy = self._energy_limit * self._game_logic.count_kind(type(self))
