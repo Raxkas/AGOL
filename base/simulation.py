5596c0d9eab783e124043896f3ee13fa94d7339e
Raxkas <rxrhhx@gmail.com>
2019-11-08 21:05:36 +0000

    Add annotations for methods returns in "Simulation" class

--- base/simulation.py
+++ base/simulation.py
@@ -1,13 +1,17 @@
 from abc import ABCMeta, abstractmethod
+from typing import Any, Tuple
+
+
+_OutputData = Any
 
 
 class Simulation(metaclass=ABCMeta):
 
     @abstractmethod
-    def next_tick(self, input_data):
+    def next_tick(self, input_data) -> _OutputData:
         pass
 
     @classmethod
     @abstractmethod
-    def initial_next_tick(cls, input_data):
+    def initial_next_tick(cls, input_data) -> Tuple["Simulation", _OutputData]:
         pass
