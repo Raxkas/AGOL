3b93c79f3bc380fc794b37adbb3cf9122d99cf2c
Raxkas <rxrhhx@gmail.com>
2019-11-08 20:41:29 +0000

    Redesign Kernel

--- base/kernel.py
+++ base/kernel.py
@@ -1,13 +1,15 @@
 class Kernel:
 
-    def __init__(self, next_tick_function):
-        self._next_tick_function = next_tick_function
-        self._simulation_state = None
+    def __init__(self, simulation_class):
+        self._simulation_class = simulation_class
+        self._simulation = None
         self._accumulated_input_data = []
 
     def next_tick(self, input_data):
         self._accumulated_input_data.append(input_data)
-        self._simulation_state, output_data = self._next_tick_function(
-            self._simulation_state, input_data
-        )
+        if self._simulation is None:
+            self._simulation, output_data = \
+                self._simulation_class.initial_next_tick(input_data)
+        else:
+            output_data = self._simulation.next_tick(input_data)
         return output_data
