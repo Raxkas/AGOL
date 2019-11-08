6c1908dbaab4380ee119a1104bf5a82b2b12a49f
Raxkas <rxrhhx@gmail.com>
2019-11-08 15:09:56 +0000

    Add "Field" class

---
+++ worlds/field.py
@@ -0,0 +1,55 @@
+from functools import lru_cache
+from itertools import product
+
+from base.world import World
+
+
+class Field(World):
+    # TODO: doc
+
+    def __init__(self, *, size):
+        super().__init__()
+        size = tuple(size)
+        if len(size) != 2:
+            raise IndexError("Field size consists of 2 elements")
+        if set(map(type, size)) != {int}:
+            raise TypeError("Field size consists of int objects")
+        self._size = size
+        size_x, size_y = size
+        self._cells_content = tuple(
+            [None] * size_x for y in range(size_y)
+        )
+        cells = product(range(size_x), range(size_y))
+        cells = self.__get_sorted_cells(cells)
+        cells = tuple(cells)
+        self._cells = cells
+
+    @property
+    def cells(self):
+        return self._cells
+
+    @staticmethod
+    def __get_sorted_cells(cells):
+        return sorted(cells, key=lambda cell: (cell[1], cell[0]))
+
+    # TODO: maybe should be implemented in subclasses
+    #       or depending on special property "neighbors_pattern"
+    @lru_cache(maxsize=None)
+    def get_adjacent_cells(self, cell):
+        x, y = cell
+        size_x, size_y = self._size
+        neighbors_offsets = product(range(-1, +1 + 1), repeat=2)
+        neighbors_offsets = self.__get_sorted_cells(neighbors_offsets)
+        result = tuple(
+            ((x + dx) % size_x, (y + dy) % size_y)
+            for dx, dy in neighbors_offsets
+        )
+        return result
+
+    def get_cell_content(self, cell):
+        x, y = cell
+        return self._cells_content[y][x]
+
+    def set_cell_content(self, cell, new_content):
+        x, y = cell
+        self._cells_content[y][x] = new_content
