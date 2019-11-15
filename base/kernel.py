aa9ec351a23698204bfac1b6abd916efaefbc1a0
Raxkas <rxrhhx@gmail.com>
2019-11-15 20:21:52 +0000

    Remove input_data accumulation

--- base/kernel.py
+++ base/kernel.py
@@ -1,12 +1,12 @@
+# TODO: Remove Kernel class?
 class Kernel:
 
     def __init__(self, simulation_class):
         self._simulation_class = simulation_class
         self._simulation = None
-        self._accumulated_input_data = []
+        # TODO: input_data/output_data observer?
 
     def next_tick(self, input_data):
-        self._accumulated_input_data.append(input_data)
         if self._simulation is None:
             self._simulation, output_data = \
                 self._simulation_class.initial_next_tick(input_data)
