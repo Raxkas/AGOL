be0af42604ed00b1bf1e7aaccbf72d34d4358082
Raxkas <rxrhhx@gmail.com>
2019-12-20 18:17:57 +0000

    Add Field.size attr

--- worlds/field.py
+++ worlds/field.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from functools import lru_cache
 from itertools import product
 
@@ -7,6 +8,8 @@ from base.world import World
 class Field(World):
     # TODO: doc
 
+    _Size = namedtuple("Size", "x y")
+
     def __init__(self, *, size):
         super().__init__()
         size = tuple(size)
@@ -14,7 +17,7 @@ class Field(World):
             raise IndexError("Field size consists of 2 elements")
         if set(map(type, size)) != {int}:
             raise TypeError("Field size consists of int objects")
-        self._size = size
+        self.size = self._Size(*size)
         size_x, size_y = size
         self._cells_content = tuple(
             [None] * size_x for y in range(size_y)
@@ -34,10 +37,11 @@ class Field(World):
 
     # TODO: maybe should be implemented in subclasses
     #       or depending on special property "neighbors_pattern"
+    # TODO: maybe should precompute and use 2d list instead of dict
     @lru_cache(maxsize=None)
     def get_adjacent_cells(self, cell):
         x, y = cell
-        size_x, size_y = self._size
+        size_x, size_y = self.size
         neighbors_offsets = product(range(-1, +1 + 1), repeat=2)
         neighbors_offsets = self.__get_sorted_cells(neighbors_offsets)
         result = tuple(
