4efbf0c2fa21755adfea01dab4d0d75d91f25a9c
Raxkas <rxrhhx@gmail.com>
2020-01-28 20:38:46 +0000

    Now Tickable's constructor takes "world" argument

--- base/tickable.py
+++ base/tickable.py
@@ -6,6 +6,9 @@ from base.action import Action
 
 class Tickable(metaclass=ABCMeta):
 
+    def __init__(self, *, world):
+        self._world = world
+
     @abstractmethod
-    def next_tick(self, world, cell, random) -> Iterable[Action]:
+    def next_tick(self, cell, random) -> Iterable[Action]:
         pass
